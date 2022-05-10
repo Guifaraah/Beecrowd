48c75e68d242f948311b0ae7bf4ff8767ac0b311
Guifaraah <gdcf.pro@gmail.com>
2022-05-10 01:07:28 -0300

    .

---
+++ 1050.py
@@ -0,0 +1,7 @@
+DDD = {61: 'Brasilia', 71: 'Salvador', 11: 'Sao Paulo', 21: 'Rio de Janeiro', 32: 'Juiz de Fora', 19: 'Campinas',
+       27: 'Vitoria', 31: 'Belo Horizonte'}
+n = int(input())
+if n in DDD.keys():
+       print(DDD[n])
+else:
+       print("DDD não cadastrado")
